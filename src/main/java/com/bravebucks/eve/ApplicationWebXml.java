c5e988f3e64cdddde49d8224a49374274b9596da
Troyburn <troyburn@the-morg.org>
2021-09-12 16:22:40 +0100

    3/9 spring-boot:2.1.8.RELEASE o.s.b.web.servlet.support.*

--- src/main/java/com/bravebucks/eve/ApplicationWebXml.java
+++ src/main/java/com/bravebucks/eve/ApplicationWebXml.java
@@ -2,7 +2,7 @@ package com.bravebucks.eve;
 
 import com.bravebucks.eve.config.DefaultProfileUtil;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * This is a helper Java class that provides an alternative to creating a web.xml.
